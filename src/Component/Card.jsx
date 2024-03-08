f4402239f669e63546b5c3d14fc3190bf935266b
aman verma <amanv3093@gmai.com>
2024-03-08 21:53:02 +0530

    first commit

---
+++ src/Component/Card.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Card() {
+    let [data,setData]=useState('');
+    let [filterData,setFilterData]=useState([])
+   
+    const search = (e) => {
+        setData(e.target.value);
+      };
+
+    
+    const click = () => {
+      if(data===''){
+        toast.error('Please Provide Value', {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          
+          });
+
+      }else{
+        setFilterData([...filterData, data]);
+        setData('')
+        toast.success('Item Added To The List', {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          
+          });
+      }
+      
+        
+       
+    };
+
+    const deleteBtn = (elem,index) => {
+        let a=filterData.filter((e,idx)=>{
+           return idx!=index;
+        })
+        setFilterData(a)
+        toast.success('Item Deleted', {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          
+          });
+        
+        
+    };
+    useEffect(()=>{
+      let item=JSON.parse(localStorage.getItem('data'));
+      if(item){
+        setData(item)
+      }
+    },[])
+
+    useEffect(()=>{
+      localStorage.setItem('data', JSON.stringify(filterData))
+    },[filterData])
+
+   
+   
+
+   return (
+    <div className='box'>
+   
+    
+    <h2>Grocery Bud</h2>
+    <div className='input-MainBox'>
+        <div className='input-box'>
+        <input type='text' onChange={search} value={data}/>
+        <button className='additem' onClick={click}>Add item</button>
+        <ToastContainer
+position="top-center"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"/>
+        </div>
+        {
+            filterData.map((elem,index)=>(
+            <div className='text-box'>
+                <div className='check-box'>
+                  <input type='checkbox'/>
+                  <p>{elem}</p>
+                </div>
+                   <button className='deleteBtn' onClick={()=>deleteBtn(elem,index)}>delete</button> 
+                  
+            </div>
+            ))
+        }
+      
+    </div>
+      
+    </div>
+  )
+}
+
+export default Card
