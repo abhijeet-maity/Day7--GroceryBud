a76495fbf066ffda2d584cd17475961ab1e79db1
abhijeet-maity <abhijeetmaity1000@gmail.com>
2024-03-13 23:16:40 +0530

    fixed the code

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -3,40 +3,43 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Card() {
-    const [data, setData] = useState('');
-    const [filterData, setFilterData] = useState([]);
-    const [checkList, setCheckList] = useState([]);
+    const [data, setData] = useState(''); //changes in the search data.
+    const [localStoragedData, setLocalStorage] = useState([]);//changes in the list data to filter localStorage.
+    const [checkList, setCheckList] = useState([]);//changes in the checklist data.
 
+    // Retrieve previos data from localStorage when component loads in UI.
     useEffect(() => {
-        // Retrieve data from localStorage when component mounts
         const storedData = localStorage.getItem('data');
         if (storedData) {
-            setFilterData(JSON.parse(storedData));
+            setLocalStorage(JSON.parse(storedData));
         }
     }, []);
 
-    useEffect(() => {
-        // Update localStorage whenever filterData changes
-        localStorage.setItem('data', JSON.stringify(filterData));
-    }, [filterData]);
 
+    // Update localStorage whenever we delete something from the list.
+    useEffect(() => {
+        localStorage.setItem('data', JSON.stringify(localStoragedData));
+    }, [localStoragedData]);
+
+
+    //get the data from the search bar.
     const search = (e) => {
         setData(e.target.value);
     };
 
-    const click = () => {
+    const addItemToList = () => {
         if (data.trim() === '') {
             toast.error('Please provide a value', { position: 'top-center' });
         } else {
-            setFilterData([...filterData, data]);
+            setLocalStorage([...localStoragedData, data]);
             setData('');
             toast.success('Item added to the list', { position: 'top-center' });
         }
     };
 
-    const deleteBtn = (elem, index) => {
-        const newFilterData = filterData.filter((e, idx) => idx !== index);
-        setFilterData(newFilterData);
+    const deleteItem = (elem, index) => {
+        const newFilterData = localStoragedData.filter((e, idx) => idx !== index);
+        setLocalStorage(newFilterData);
         toast.success('Item deleted', { position: 'top-center' });
     };
 
@@ -52,16 +55,16 @@ function Card() {
             <div className='input-MainBox'>
                 <div className='input-box'>
                     <input type='text' onChange={search} value={data} />
-                    <button className='additem' onClick={click}>Add item</button>
+                    <button className='additem' onClick={addItemToList}>Add item</button>
                     <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} />
                 </div>
-                {filterData.map((elem, index) => (
+                {localStoragedData.map((elem, index) => (
                     <div className='text-box' key={index}>
                         <div className='check-box'>
                             <input type='checkbox' checked={checkList[index] || false} onChange={() => toggleCheck(index)} />
                             <p className='para' style={{ textDecoration: checkList[index] ? 'line-through' : 'none' }}>{elem}</p>
                         </div>
-                        <button className='deleteBtn' onClick={() => deleteBtn(elem, index)}>Delete</button>
+                        <button className='deleteItem' onClick={() => deleteItem(elem, index)}>Delete</button>
                     </div>
                 ))}
             </div>
